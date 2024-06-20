4f98b5665152d7e3b14293488a4104635b56f3b5
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-06-20 15:57:45 +0530

    complete configs for webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode:'development',
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'todo-list',
+      template: './src/index.html',
+      inject:'body'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          outputPath: 'asset/',
+          publicPath: 'asset/'
+        },
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+};
