cc423a793e6cd70afa2c9c4959a0070dffa125e7
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-06-28 02:53:54 +0530

    feat: complete event listener func for task and project

---
+++ src/addDOM.js
@@ -0,0 +1,97 @@
+import './style.css';
+import { addProject } from './addProject';
+import { getTask } from './getData';
+import { addTask } from './addTask';
+import { updateTaskList } from './updateUI';
+
+export let currentProject = "DEFAULT PROJECT";
+
+
+export function addEventListener(){
+
+    addTaskListener();
+    addProjectListener();
+    selectedProject();
+
+} 
+
+
+function addTaskListener(){
+    const task = document.querySelector('.task-btn')
+    const taskModal = document.querySelector('#task-modal')
+    const close_btn = document.querySelector("#task-modal .close-modal");
+    const modalForm = document.querySelector('#taskModalForm');
+
+    close_btn.addEventListener('click',()=>{
+        taskModal.style.display="none";
+    });
+
+
+    task.addEventListener('click', () => {
+        taskModal.style.display = 'flex';
+      });
+
+      window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+          taskModal.style.display = 'none';
+        }
+      });
+    
+      modalForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        addTask();
+        taskModal.style.display = 'none';
+      });
+}
+
+
+
+function addProjectListener(){
+    const openModal = document.querySelector('.project-btn');
+    const closeModal = document.querySelector('.project-close-modal');
+    const modal = document.querySelector('#modal');
+    const modalForm = document.querySelector('#projectModalForm');
+
+    openModal.addEventListener('click', () => {
+        modal.style.display = 'flex';
+      });
+
+    
+      closeModal.addEventListener('click', () => {
+        modal.style.display = 'none';
+      });
+  
+      modalForm.addEventListener('submit',(event)=>{
+        event.preventDefault();
+        addProject();
+        modal.style.display = 'none';
+      })
+}
+
+
+export function selectedProject(){
+    const projects = document.querySelector('.projects');
+    projects.addEventListener('click',(e)=>{
+        if(e.target && e.target.nodeName == "LI"){
+            const items = document.querySelectorAll('.projects li');
+            items.forEach(item => item.classList.remove('selected'));
+            e.target.classList.add('selected');
+            currentProject = e.target.textContent;
+            updateTaskList(currentProject);
+        }
+    })
+}
+
+
+
+// function taskListDisplay(){
+//     const projects = document.querySelector('.projects');
+//     projects.addEventListener('click',(e)=>{
+//         if(e.target && e.target.nodeName == "LI"){
+//             const items = document.querySelectorAll('.projects li');
+//             items.forEach(item => item.addEventListener());
+//             e.target.classList.add('selected');
+//             currentProject = e.target.textContent;
+//         }
+//     })
+// }
