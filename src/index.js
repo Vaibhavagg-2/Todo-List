018f17ce527e2e9095d15935830856f872e0fb16
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-06-28 02:50:11 +0530

    optimize: reorganize js logic into separate modules

--- src/index.js
+++ src/index.js
@@ -1,47 +1,13 @@
 import './style.css';
+import { addEventListener } from './addDOM';
+import { projects,getProjects,storeProjects } from './storage';
+import { updateProjectList } from './updateUI';
 
 
-const openModal = document.querySelector('.project-btn');
-const task = document.querySelector('.task-btn')
-const closeModal = document.getElementById('closeModal');
-const modal = document.getElementById('modal');
-const taskModal = document.querySelector('#task-modal')
-const modalForm = document.getElementById('modalForm');
+function app(){
+  getProjects();
+  updateProjectList(projects);
+  addEventListener();
+}
 
-
-const close_btn = document.querySelector("#task-modal .close-modal");
-
-close_btn.addEventListener('click',()=>{
-    taskModal.style.display="none";
-});
-
-
-
-    openModal.addEventListener('click', () => {
-      modal.style.display = 'flex';
-    });
-
-    task.addEventListener('click', () => {
-        taskModal.style.display = 'flex';
-      });
-
-
-    closeModal.addEventListener('click', () => {
-      modal.style.display = 'none';
-    });
-  
-    window.addEventListener('click', (event) => {
-      if (event.target === modal) {
-        modal.style.display = 'none';
-      }
-    });
-  
-    modalForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      alert('Form submitted!');
-      modal.style.display = 'none';
-    });
-  
-
-
-
+app();
